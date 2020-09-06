114b961ff0e7dd0fd7beb8c3e7c8983b99e09705
Robertokbr <robertojuniordev@gmail.com>
2020-09-06 00:02:33 -0300

    Add Routes config

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,62 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View } from 'react-native';
+import { AntDesign, Ionicons } from 'expo-vector-icons';
 import Home from '../pages/Home';
 import Wallet from '../pages/Wallet';
+import PayButton from '../components/PayButton/index';
+
+const icons = {
+  Home: {
+    lib: AntDesign,
+    name: 'home',
+  },
+  Wallet: {
+    lib: AntDesign,
+    name: 'creditcard',
+  },
+  Notifications: {
+    lib: Ionicons,
+    name: 'ios-notifications-outline',
+  },
+  Settings: {
+    lib: AntDesign,
+    name: 'setting',
+  },
+};
+
+interface RoutesProps {
+  name: string;
+}
 
 const Tab = createBottomTabNavigator();
 
 const Routes: React.FC = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route, navigation }) => ({
+        tabBarIcon: ({ color, size, focused }) => {
+          if (route.name === 'Pay') {
+            return (
+              <PayButton
+                focused={focused}
+                onPress={() => navigation.navigate('Pay')}
+              />
+            );
+          }
+          const { lib: Icon, name } = icons[route.name];
+          return <Icon name={name} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        style: {
+          backgroundColor: '#131418',
+          borderTopColor: 'rgba(255,255,255,0.2)',
+        },
+        activeTintColor: '#FFF',
+        inactiveTintColor: '#92929c',
+      }}
+    >
       <Tab.Screen
         name="Home"
         component={Home}
@@ -20,6 +69,27 @@ const Routes: React.FC = () => {
         component={Wallet}
         options={{
           title: 'Carteira',
+        }}
+      />
+      <Tab.Screen
+        name="Pay"
+        component={View}
+        options={{
+          title: '',
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={View}
+        options={{
+          title: 'Notificações',
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={View}
+        options={{
+          title: 'Settings',
         }}
       />
     </Tab.Navigator>
