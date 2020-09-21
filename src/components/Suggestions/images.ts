2bd5eed9746f166d90f6e2a01268da888b08d160
robertokbr <robertojuniordev@gmail.com>
2020-09-21 20:54:22 -0300

    Add array of Options objects content

---
+++ src/components/Suggestions/images.ts
@@ -0,0 +1,45 @@
+import img01 from '../../assets/01.png';
+import img02 from '../../assets/02.png';
+import img03 from '../../assets/03.png';
+import img04 from '../../assets/04.png';
+import img05 from '../../assets/05.png';
+import img06 from '../../assets/06.png';
+import img07 from '../../assets/07.png';
+
+export default [
+  {
+    key: String(Math.random()),
+    img: img01,
+    label: 'Recarga',
+  },
+  {
+    key: String(Math.random()),
+    img: img02,
+    label: 'Uber',
+  },
+  {
+    key: String(Math.random()),
+    img: img03,
+    label: 'Ônibus',
+  },
+  {
+    key: String(Math.random()),
+    img: img04,
+    label: 'Tv',
+  },
+  {
+    key: String(Math.random()),
+    img: img05,
+    label: 'Doações',
+  },
+  {
+    key: String(Math.random()),
+    img: img06,
+    label: 'Cód. Barras',
+  },
+  {
+    key: String(Math.random()),
+    img: img07,
+    label: 'FAQ',
+  },
+];
