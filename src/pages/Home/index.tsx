c98c18acfce6cc92eb56827f1b23b5910084d7e2
robertokbr <robertojuniordev@gmail.com>
2020-09-23 23:55:34 -0300

    Add new Components into Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { MaterialCommunityIcons, AntDesign } from 'expo-vector-icons';
 
-import { Container } from './styles';
+import * as S from './styles';
+import Suggestions from '../../components/Suggestions';
+import Activities from '../../components/Activities';
 
 const Home: React.FC = () => {
   return (
-    <Container>
-      <Text>Home</Text>
-    </Container>
+    <S.Wrapper>
+      <S.Container keyboardShouldPersistTaps="handled">
+        <S.Header>
+          <MaterialCommunityIcons
+            name="qrcode-scan"
+            color="#10c86e"
+            size={30}
+          />
+          <S.BalanceContainer>
+            <S.BalanceTitle>Meu saldo</S.BalanceTitle>
+            <S.Balance>R$1254k</S.Balance>
+          </S.BalanceContainer>
+          <AntDesign name="gift" color="#10c86e" size={30} />
+        </S.Header>
+        <Suggestions />
+        <Activities />
+      </S.Container>
+    </S.Wrapper>
   );
 };
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,33 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView``;
+export const Wrapper = styled.SafeAreaView`
+  flex: 1;
+  background: #000000;
+`;
+
+export const Container = styled.ScrollView.attrs(() => ({
+  contentContainerStyle: {},
+}))``;
+
+export const Header = styled.View`
+  height: 50px;
+  padding: 0 16px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const BalanceContainer = styled.View``;
+
+export const BalanceTitle = styled.Text`
+  color: rgba(255, 255, 255, 0.8);
+  font-size: 14px;
+  text-align: center;
+`;
+
+export const Balance = styled.Text`
+  color: #fff;
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+`;
