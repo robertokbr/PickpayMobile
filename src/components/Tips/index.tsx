8c37e543359802d9e3a63810786717013e9d498e
robertokbr <robertojuniordev@gmail.com>
2020-09-23 23:55:14 -0300

    Add Component Tips

---
+++ src/components/Tips/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Tips: React.FC = () => {
+  return (
+    <Container>
+      <Text>Tips</Text>
+    </Container>
+  );
+};
+
+export default Tips;
